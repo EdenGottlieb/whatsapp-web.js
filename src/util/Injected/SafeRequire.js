6ed48444b4674cefc29eea5aae1b931f3a5f4617
Eden Gottlieb <eden.gottlieb@noiser.io>
2026-08-23 17:23:57 +0300

    fix safeRequire

--- src/util/Injected/SafeRequire.js
+++ src/util/Injected/SafeRequire.js
@@ -2,11 +2,6 @@
 
 exports.LoadSafeRequire = () => {
     window.safeRequire = (moduleName) => {
-        try {
-            return window.require(moduleName)
-        } catch (e) {
-            console.log('Error while requiring module, resuming safely. Error: ', e)
-            return {}
-        }
+            return window.require(moduleName) ?? {}
     }
 }
